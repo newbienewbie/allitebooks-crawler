6e230d4ac56cbda391159728a6f11ce13e02efa1
newbienewbie <webbot@sina.com>
2017-04-29 13:24:13 +0800

    重构：重新安排文件夹布局

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const Crawler=require('./lib/crawler');
+const SchemeCrawler=require('./lib/crawler/scheme-crawler/crawler');
 const url=require('url');
 
 
 
-// URL 构成:
-// - esf : 二手房
-// cz : 常州
 const base="http://www.allitebooks.com/";
 // 新建爬虫
-const crawler=new Crawler(base);
+const schemeCrawler=new SchemeCrawler(base);
 
 // 启动脚本时初次抓取抓取
-crawler.crawl(base);
+schemeCrawler.crawl(base);
 
 // 定期抓取
 setInterval(
-    function(){ crawler.crawl(base); },
+    function(){ schemeCrawler.crawl(base); },
     // 每隔4小时抓取一次
     4*60*60*1000
 );
