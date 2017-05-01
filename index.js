381faeba7828aa325110af44424220a0b0eeba7f
newbienewbie <webbot@sina.com>
2017-05-01 10:29:18 +0800

    启动：改为两个独立命令

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const {promiseWait,processScheme,processDetail,scheduleScheme,scheduleDetail}=require('./lib/task');
 
+const args=process.argv.slice(1);
 
+const _filename=args.shift();
+const mode=args.shift();
 
-scheduleScheme(24*60*60*1000);
+switch(mode){
+    case "scheme":
+        scheduleScheme(24*60*60*1000);
+        break;
+    case "detail":
+        scheduleDetail(5,4*60*1000);
+        break;
+    default:
+        console.log(`syntax: node index.js [mode]`);
+        break;
+}
 
-scheduleDetail(5,4*60*1000);
