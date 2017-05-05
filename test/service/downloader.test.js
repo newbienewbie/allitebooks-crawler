326de283918a0d4b5a01305191a6d02bdcd8d9f5
newbienewbie <webbot@sina.com>
2017-05-05 10:44:52 +0800

    为下载器的步进监听添加总大小参数

--- test/service/downloader.test.js
+++ test/service/downloader.test.js
@@ -4,10 +4,11 @@ const {download}=require('../../lib/service/downloader');
 describe('downloader tester',function(done){
     it('#download() jpg',function(){
         this.timeout(50000);
-        let totalSize=0;
-        return download("http://www.allitebooks.com/wp-content/uploads/2015/04/Foundation-HTML5-with-CSS3.jpeg",__dirname,chunck=>{
-            totalSize+=Buffer.from(chunck).length;
-            console.log(`current downloaded size:${parseInt(totalSize/1024)} KB`);
+        let accumulated=0;
+        let url="http://www.allitebooks.com/wp-content/uploads/2015/04/Foundation-HTML5-with-CSS3.jpeg";
+        return download(url,__dirname,(chunck,total)=>{
+            accumulated+=Buffer.from(chunck).length;
+            console.log(`percentage: ${accumulated/total*100} %`);
         });
     });
     it('#download() pdf',function(){
