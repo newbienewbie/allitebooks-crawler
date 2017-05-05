bef61c84163d7b74f6a6b7b710fbb668ec521383
newbienewbie <webbot@sina.com>
2017-05-05 10:20:44 +0800

    为downloader添加步进监听

--- test/service/downloader.test.js
+++ test/service/downloader.test.js
@@ -3,7 +3,12 @@ const {download}=require('../../lib/service/downloader');
 
 describe('downloader tester',function(done){
     it('#download() jpg',function(){
-        return download("http://www.allitebooks.com/wp-content/uploads/2015/04/Foundation-HTML5-with-CSS3.jpeg",__dirname);
+        this.timeout(50000);
+        let totalSize=0;
+        return download("http://www.allitebooks.com/wp-content/uploads/2015/04/Foundation-HTML5-with-CSS3.jpeg",__dirname,chunck=>{
+            totalSize+=Buffer.from(chunck).length;
+            console.log(`current downloaded size:${parseInt(totalSize/1024)} KB`);
+        });
     });
     it('#download() pdf',function(){
         this.timeout(500000);
